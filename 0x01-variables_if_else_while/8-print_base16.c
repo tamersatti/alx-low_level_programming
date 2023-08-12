54d0bfc1e3d942ba58f4a7ab661f78c4f1fed42c
tamersatti <tamer.satti@gmail.com>
2023-08-11 17:20:09 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,11 @@ int main(void)
 
 	c = 'a';
 	d = 0;
-	 while
-                (d < 10) {
-                        putchar(d + '0');
-                        d++;
-                }
+	while
+               (d < 10) {
+                       putchar(d + '0');
+                       d++;
+               }
 	while
 		(c <= 'f') {
 			putchar(c);
