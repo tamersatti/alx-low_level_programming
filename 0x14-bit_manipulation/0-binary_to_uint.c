15c188e43f4c6cf03b7cc547e7095edd6c189224
tamirsatti <tamer.satti@gmail.com>
2023-09-29 04:21:23 -0700

    update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint convert  binary number to unsigned int
+ * bin string containing binary number
+ *
+ * Return the converted number
+ */
+unsigned int binary_to_uint(const char *bin)
+{
+	int in;
+	unsigned int dec_value = 0;
+
+	if (!bin)
+		return (0);
+
+	for (in = 0; bin[in]; in++)
+	{
+		if (bin[in] < '0' || bin[in] > '1')
+			return (0);
+		dec_value = 2 * dec_value + (bin[in] - '0');
+	}
+
+	return (dec_value);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *bin);
+void print_bin(unsigned long int in);
+int gets_bit(unsigned long int in, unsigned int ind);
+int sets_bit(unsigned long int *in, unsigned int ind);
+int clears_bit(unsigned long int *in, unsigned int ind);
+unsigned int flips_bit(unsigned long int in, unsigned long int im);
+int _atoi(const char *is);
+int _putchar(char ch);
+int get_endian(void);
+
+#endif
